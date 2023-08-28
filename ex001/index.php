8d905a16a217a370dd5c2c19bc24ff57a1b623a9
Ruan Henrique <101962124+selogerkkk@users.noreply.github.com>
2023-08-28 03:58:53 -0300

    fix: local folha de estilo

--- ex001/index.php
+++ ex001/index.php
@@ -5,16 +5,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="/phpstudy/style.css">
 </head>
 
 <body>
     <main>
         <h2>Calculadora do sexo</h2>
         <?php
-    $valor1 = $_GET['v1'] ?? 0;
-    $valor2 = $_GET['v2'] ?? 0;
-    ?>
+        $valor1 = $_GET['v1'] ?? 0;
+        $valor2 = $_GET['v2'] ?? 0;
+        ?>
         <form action="<?= $_SERVER['PHP_SELF'] ?>" method="get">
             <label for="v1">Valor 1</label>
             <input type="number" name="v1" id="v1" value="<?= $valor1 ?>">
@@ -26,9 +26,9 @@
     <section>
         <h2>Resultado da soma</h2>
         <?php
-    $soma = $valor1 + $valor2;
-    print("A soma de $valor1 e $valor2 é: <strong>$soma</strong>");
-    ?>
+        $soma = $valor1 + $valor2;
+        print("A soma de $valor1 e $valor2 é: <strong>$soma</strong>");
+        ?>
     </section>
 </body>
 
